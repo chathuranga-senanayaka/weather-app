d6ff981dd3f531a20a63e94c523910267f45e2b5
Chathuranga-Senanayaka <senanayakacjs@gmail.com>
2025-03-16 16:57:53 +0530

    installed shadcn button input and card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-sky-400 to-blue-500 p-4 flex items-center justify-center">
+      <div className="w-full max-w-md space-y-4"></div>
+    </div>
+  );
 }
