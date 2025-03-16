25fe8bd46f091cb77f0a7283546409d0f8cb8994
Chathuranga-Senanayaka <senanayakacjs@gmail.com>
2025-03-16 19:25:56 +0530

    installed zod

--- app/action.ts
+++ app/action.ts
@@ -15,6 +15,9 @@ export async function getWeatherData(
     const res = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHERMAP_API_KEY}`
     );
+    if (!res.ok) {
+      throw new Error("City not found");
+    }
     const data = await res.json();
     return { data };
   } catch (error) {
