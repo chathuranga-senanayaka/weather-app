25534ca1aa653ee3034f2e90d72c86ab879361ee
Chathuranga-Senanayaka <senanayakacjs@gmail.com>
2025-03-17 08:55:42 +0530

    get ready for deploy in gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+const isProd = process.env.NODE_ENV === "production";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/weather-app/" : "",
+  basePath: isProd ? "/weather-app" : "",
+  output: "export",
 };
 
 export default nextConfig;
