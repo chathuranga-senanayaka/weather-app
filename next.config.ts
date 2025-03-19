60435eb4bc342eb3e3e5933765ae4abd5fcd9271
Chathuranga-Senanayaka <senanayakacjs@gmail.com>
2025-03-19 22:50:12 +0530

    fixed next.config

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,15 @@ const isProd = process.env.NODE_ENV === "production";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {},
+  },
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
   assetPrefix: isProd ? "/weather-app/" : "",
   basePath: isProd ? "/weather-app" : "",
-  output: "export",
 };
 
 export default nextConfig;
